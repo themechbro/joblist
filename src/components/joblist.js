826c9474e8473e5fd428fd313633e93d3f7a13b5
themechbro <adrinpaul29@gmail.com>
2024-05-05 10:24:13 +0530

    "Added infinite scrolling to JobList component using useState and useEffect hooks, and cleaned up code"
    - Added a new state variable `offset

--- src/components/joblist.js
+++ src/components/joblist.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchJobs } from '../actions/jobActions';
 import Card from '@mui/material/Card';
@@ -8,7 +8,6 @@ import CardActions from '@mui/material/CardActions';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import './joblist.css'
@@ -17,29 +16,45 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 function JobList() {
   const dispatch = useDispatch();
   const jobs = useSelector(state => state.jobs.items[0]);
+  const [offset, setOffset]= useState(0);
   console.log(jobs);
 
   useEffect(() => {
-    dispatch(fetchJobs(10,0));
-  }, [dispatch]);
+    dispatch(fetchJobs(10,offset));
+  }, [dispatch, offset]);
+
+  function handleScroll(){
+    //if condition checks if the user has moved down or not. 
+    //window.innerHeight is the height of the viewport, 
+    //document.documentElement.scrollTop is how much the user has scrolled vertically, and 
+    //document.documentElement.offsetHeight is the total height of the content of the page. 
+    //If the user hasn’t scrolled to the bottom of the page, the function returns immediately and does nothing. 
+    if (window.innerHeight + document.documentElement.scrollTop !==   
+       document.documentElement.offsetHeight) return;
+       setOffset(prevOffset=> prevOffset+10);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll);
+    return ()=> window.removeEventListener('scroll',handleScroll); //removes the event listener when the component unmounts. This is important to prevent memory leaks.
+},[]);
 
   return (
     <div className='deck'>
       {jobs.map(job => (
         <Card sx={{ maxWidth: 345, marginBottom:2, marginTop:1}}>
-      <CardHeader
+      <CardHeader sx={{display:'flex', flexDirection:'column',alignItems:'flex-start'}}
         avatar={
           <Avatar  aria-label="jobs">
             <img src={job.logoUrl}/>
           </Avatar>
         }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
+        
         title={job.jobRole}
         subheader={job.location}
+        header={ <Typography variant="body3" color="text.secondary">
+          Estimated Salary : {job.experience}
+        </Typography>}
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
